aadd5935eb4c1143a3418ae73719c281ffd53ef1
joel <joelarcos@polarityte.com>
2019-02-20 09:42:45 -0700

    test commit

--- InterviewPrep/AppDelegate.swift
+++ InterviewPrep/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("hi")
         // Override point for customization after application launch.
         return true
     }
